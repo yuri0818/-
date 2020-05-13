61342e4e6413d956d683c63cc2b2d3aef91e5790
yuri0818 <yyyy.0818.0830@gmail.com>
2020-05-13 10:23:55 +0000

    Finish add-basic-info

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,23 @@
 class User < ApplicationRecord
+  # 「remember_token」という仮想の属性を作成します。
+  attr_accessor :remember_token
   before_save { self.email = email.downcase }
 
   validates :name,  presence: true, length: { maximum: 50 }
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 100 },
                     format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: true    
+                    uniqueness: true
+  validates :department, length: { in: 2..30 }, allow_blank: true
+  
+  validates :basic_time, presence: true
+  validates :work_time, presence: true
+  
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
-# 渡された文字列のハッシュ値を返します。
+  # 渡された文字列のハッシュ値を返します。
   def User.digest(string)
     cost = 
       if ActiveModel::SecurePassword.min_cost
@@ -24,24 +32,23 @@ class User < ApplicationRecord
   def User.new_token
     SecureRandom.urlsafe_base64
   end
-  
-  
+
   # 永続セッションのためハッシュ化したトークンをデータベースに記憶します。
   def remember
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
   end
-  
-   # トークンがダイジェストと一致すればtrueを返します。
+
+  # トークンがダイジェストと一致すればtrueを返します。
   def authenticated?(remember_token)
     # ダイジェストが存在しない場合はfalseを返して終了します。
     return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
- 
- 
- # ユーザーのログイン情報を破棄します。
+
+  # ユーザーのログイン情報を破棄します。
   def forget
     update_attribute(:remember_digest, nil)
   end
 end
+
