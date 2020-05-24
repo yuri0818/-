7701bd1741abce1c5dd46612d55a39f436547fb4
yuri0818 <yyyy.0818.0830@gmail.com>
2020-05-24 14:29:10 +0000

    勤怠編集更新ne

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -3,7 +3,8 @@ class AttendancesController < ApplicationController
   before_action :logged_in_user, only: [:update, :edit_one_month]
   before_action :admin_or_correct_user, only: [:update, :edit_one_month, :update_one_month]
   before_action :set_one_month, only: :edit_one_month
-
+  before_action :started_at_is_invalid_without_a_finished_at?, only: :update_one_month
+ 
   UPDATE_ERROR_MSG = "勤怠登録に失敗しました。やり直してください。"
 
   def update
@@ -31,17 +32,23 @@ class AttendancesController < ApplicationController
 
   def update_one_month
     ActiveRecord::Base.transaction do # トランザクションを開始します。
-      attendances_params.each do |id, item|
-        attendance = Attendance.find(id)
-        attendance.update_attributes!(item)
-      end
+      if started_at_is_invalid_without_a_finished_at? 
+        attendances_params.each do |id, item|
+          attendance = Attendance.find(id)
+          attendance.update_attributes!(item)
+        end 
+        flash[:success] = "1ヶ月分の勤怠情報を更新しました。"
+        redirect_to user_url(date: params[:date])
+      else
+       flash[:danger] = "無効な入力データがあった為、更新をキャンセルしました。"
+       redirect_to attendances_edit_one_month_user_url(date: params[:date]) 
+      end  
     end
-    flash[:success] = "1ヶ月分の勤怠情報を更新しました。"
-    redirect_to user_url(date: params[:date])
   rescue ActiveRecord::RecordInvalid # トランザクションによるエラーの分岐です。
     flash[:danger] = "無効な入力データがあった為、更新をキャンセルしました。"
-    redirect_to attendances_edit_one_month_user_url(date: params[:date])
+    redirect_to attendances_edit_one_month_user_url(date: params[:date]) 
   end
+
 
   private
 
@@ -53,11 +60,22 @@ class AttendancesController < ApplicationController
     # beforeフィルター
 
     # 管理権限者、または現在ログインしているユーザーを許可します。
-    def admin_or_correct_user
-      @user = User.find(params[:user_id]) if @user.blank?
-      unless current_user?(@user) || current_user.admin?
-        flash[:danger] = "編集権限がありません。"
-        redirect_to(root_url)
-      end  
-    end
+  def admin_or_correct_user
+    @user = User.find(params[:user_id]) if @user.blank?
+    unless current_user?(@user) || current_user.admin?
+      flash[:danger] = "編集権限がありません。"
+      redirect_to(root_url)
+    end  
+  end
+    
+    
+  def started_at_is_invalid_without_a_finished_at?
+    attendances_params.each do |id, item|
+      if item[:started_at].present? && item[:finished_at].blank?
+       flash[:danger] = "無効な入力データがあった為、更新をキャンセルしました。"
+       redirect_to attendances_edit_one_month_user_url(date: params[:date]) 
+      end
+    end 
+  end
+    
 end
